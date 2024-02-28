bb0968b6d94e10f6d702b999551accb923288ae0
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2024-02-28 22:02:37 +0300

    fix url

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('list/', UserListAPIView.as_view(), name='users'),
-    path('register/', UserCreate.as_view(), name='user_register'),
+    path('register/', UserCreateAPIView.as_view(), name='user_register'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user'),
     path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
     path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete')
